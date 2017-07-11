1745ebb7c82f50429e915cdff8ee5d35f5002e80
danielvivacqua <drvivacqua@hotmail.com>
2017-07-11 15:26:32 -0400

    Added classes

--- AccessModifierPractice/Boat.cs
+++ AccessModifierPractice/Boat.cs
@@ -8,5 +8,37 @@ namespace AccessModifierPractice
 {
     class Boat
     {
+        //So far we've done fields as private, but we can do them as public
+        public int numberOfPassengers;
+
+        //Most of the time, though, we want our fields to be private
+        private string name;
+        private string engineSize;
+
+        //Properties almost always have public as their access modifier
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //Constructors always have a public access modifier
+        public Boat()
+        {
+            //this is my default constructor
+        }
+
+        //Methods
+        public void Move()
+        {
+            //code to make the boat move
+        }
+
+        private double CalculateMPG()
+        {
+            double MPG = 20.0;
+            //code to calculate the MPG
+            return MPG;
+        }
     }
 }
--- AccessModifierPractice/Program.cs
+++ AccessModifierPractice/Program.cs
@@ -10,6 +10,18 @@ namespace AccessModifierPractice
     {
         static void Main(string[] args)
         {
+            Boat theBoat = new Boat();
+
+            theBoat.numberOfPassengers = 345;
+
+            //Work in pairs
+            //On paper or in a word processor
+            //Create a class cell phone and determine the
+            //3 fields
+            //2 properties
+            //4 methods
+            //For each of the members you created, determine whether it should be
+            //public or private
         }
     }
 }
